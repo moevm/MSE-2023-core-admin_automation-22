071c63966619043ecf66c22066368923c237ad5a
Yurii Kondratov <71669375+YuriiKondratov@users.noreply.github.com>
2023-03-06 21:34:00 +0300

    discord4j dependency and configuration added (#13)
    * added base structure
    
    * fix
    
    * fix
    
    * discord4j dependency and configuration added
    
    ---------
    
    Co-authored-by: brokkko <nsstrannikova@mail.ru>
    Co-authored-by: Mokhnatkin Kirill <73700612+MaverickBattler@users.noreply.github.com>
    Co-authored-by: YuriiKondratov <yuakondratov@stud.etu.ru>

--- discord_bot/bot/src/main/java/core/bgroup/bot/config/BotConfiguration.java
+++ discord_bot/bot/src/main/java/core/bgroup/bot/config/BotConfiguration.java
@@ -2,10 +2,17 @@ package core.bgroup.bot.config;
 
 import core.bgroup.yandex.config.YandexConfiguration;
 import core.bgroup.zoom.config.ZoomConfiguration;
+import discord4j.core.DiscordClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(value = {YandexConfiguration.class, ZoomConfiguration.class})
 public class BotConfiguration {
+    @Bean
+    public DiscordClient discordClient(@Value("${discord.bot-token}") String token) {
+        return DiscordClient.create(token);
+    }
 }

5a0d3d73452b21778df050d8ef1b9de90e9d43d7
Vadsxd <vadim.sannikov.2018@mail.ru>
2023-04-19 12:05:27 +0300

    hide token

--- discord_bot/bot/src/main/java/core/bgroup/bot/config/BotConfiguration.java
+++ discord_bot/bot/src/main/java/core/bgroup/bot/config/BotConfiguration.java
@@ -6,6 +6,7 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,12 +19,14 @@ public class BotConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger( BotConfiguration.class );
 
+    @Value("${discord.bot-token}")
+    private String token;
+
     @Bean
     public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
         GatewayDiscordClient client = null;
 
         try {
-            String token = "MTA4Mjk3Njg3NjEwNjEwMDc2Ng.Gy2Pmc.99CCCc3Dsk-8NiIrpIaBTpGkIWukcYMFx1e5ts";
             client = DiscordClientBuilder.create(token)
                     .build()
                     .login()
@@ -42,9 +45,4 @@ public class BotConfiguration {
 
         return client;
     }
-
-//    @Bean
-//    public ZoomMeetingService zoomMeetingService(WebClient webClient) {
-//        return new ZoomMeetingServiceImpl(webClient);
-//    }
 }
